d3c056c4f6b644db8641b2312043b4fb19626cc6
HOR32 <161935933+HOR32@users.noreply.github.com>
2024-07-26 12:02:18 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<html>
+
+<head>
+
+	<style>
+		img{
+			width:15px;
+			height:15px;
+		}
+	</style>
+</head>
+
+<?php
+$config = parse_ini_file("config.ini");
+
+if(isset($config['background']) and isset($config['color'])){
+	echo '<body style="background-color: ' . $config['background'] . '; color: ' . $config['color'] . ';">';
+}
+
+if(isset($config['banner'])){
+	echo "<h1>" . $config['banner'] . "</h1>";
+}
+else{
+	echo"<h1> banner </h1>";
+}
+echo "<hr>";
+$d = dir(".");
+echo "path: " . $d->path . "\n";
+echo "<br>";
+
+$sound_extensions = ['mp3', 'wav', 'ogg', 'm4a', 'flac'];
+
+while(false !== ($entry = $d->read())){
+	$ext = pathinfo($entry, PATHINFO_EXTENSION);
+	if(in_array($ext, $sound_extensions)){
+		echo "<img src=soundvor-3.png >";
+	}
+	if(is_dir($entry)){
+		echo "<img src=directory_closed_cool-3.png >";
+	}
+	echo "<a href='{$entry}'>{$entry}</a><br>";
+}
+$d->close();
+echo "</body>";
+?>
+</html>
