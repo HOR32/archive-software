06a8bdf57dd0dc2f023c5548feb638c7db339107
HOR32 <161935933+HOR32@users.noreply.github.com>
2024-07-29 19:48:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -41,16 +41,12 @@ $sound_extensions = ['mp3', 'wav', 'ogg', 'm4a', 'flac'];
 $image_extensions = ['png', 'webp', 'jpg', 'jpeg'];
 while(false !== ($entry = $d->read())){
 	$ext = pathinfo($entry, PATHINFO_EXTENSION);
-	if(in_array($ext, $sound_extensions)){
-		echo "<img src=soundvor-3.png >";
-	}
-	elseif(in_array($ext, $image_extensions)){
-		echo '<img class=thumb src=' . $entry . '>';
-	}
+	
 	if(is_dir($entry)){
 		echo "<img src=directory_closed_cool-3.png >";
+		echo "<a href='{$entry}'>{$entry}</a><br>";
 	}
-	echo "<a href='{$entry}'>{$entry}</a><br>";
+	
 }
 $d->close();
 echo "</body>";
